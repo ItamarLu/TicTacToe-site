42ad30762d289210f0225bbf26393577404a24c3
Itamar <Itamarlucio19@gmail.com>
2022-11-09 15:13:49 -0300

    Added some funcionality

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-let btnClick1 = document.getElementById("btn1")
+function btnR() {
+  btn1.textContent = ""
+  btn2.textContent = ""
+  btn3.textContent = ""
+  btn4.textContent = ""
+  btn5.textContent = ""
+  btn6.textContent = ""
+  btn7.textContent = ""
+  btn8.textContent = ""
+  btn9.textContent = ""
+}
 
-console.log(btnClick1)
+function btnClick1() {
+  btn1.textContent = "X"
+}
+function btnClick2() {
+  btn2.textContent = "X"
+}
+function btnClick3() {
+  btn3.textContent = "X"
+}
+function btnClick4() {
+  btn4.textContent = "X"
+}
+function btnClick5() {
+  btn5.textContent = "X"
+}
+function btnClick6() {
+  btn6.textContent = "X"
+}
+function btnClick7() {
+  btn7.textContent = "X"
+}
+function btnClick8() {
+  btn8.textContent = "X"
+}
+function btnClick9() {
+  btn9.textContent = "X"
+}
