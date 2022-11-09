7194564089b9157f50a3962fe691d41bad014daf
Itamar <Itamarlucio19@gmail.com>
2022-11-09 16:33:28 -0300

    Added more functionalities and style

--- index.js
+++ index.js
@@ -8,32 +8,97 @@ function btnR() {
   btn7.textContent = ""
   btn8.textContent = ""
   btn9.textContent = ""
+  N = 0
 }
 
+let N = 0
+
 function btnClick1() {
-  btn1.textContent = "X"
+  if (N==0, N%2==0){
+    btn1.textContent = "X"
+  }
+  if (N%2!=0){
+    btn1.textContent = "O"
+  }
+  N += 1 
 }
 function btnClick2() {
-  btn2.textContent = "X"
+  if (N==0, N%2==0){
+    btn2.textContent = "X"
+  }
+  if (N%2!=0){
+    btn2.textContent = "O"
+  }
+  N += 1 
 }
 function btnClick3() {
-  btn3.textContent = "X"
+  if (N==0, N%2==0){
+    btn3.textContent = "X"
+  }
+  if (N%2!=0){
+    btn3.textContent = "O"
+  }
+  N += 1 
 }
 function btnClick4() {
-  btn4.textContent = "X"
+  if (N==0, N%2==0){
+    btn4.textContent = "X"
+  }
+  if (N%2!=0){
+    btn4.textContent = "O"
+  }
+  N += 1 
 }
 function btnClick5() {
-  btn5.textContent = "X"
+  if (N==0, N%2==0){
+    btn5.textContent = "X"
+  }
+  if (N%2!=0){
+    btn5.textContent = "O"
+  }
+  N += 1 
 }
 function btnClick6() {
-  btn6.textContent = "X"
+  if (N==0, N%2==0){
+    btn6.textContent = "X"
+  }
+  if (N%2!=0){
+    btn6.textContent = "O"
+  }
+  N += 1
 }
 function btnClick7() {
-  btn7.textContent = "X"
+  if (N==0, N%2==0){
+    btn7.textContent = "X"
+  }
+  if (N%2!=0){
+    btn7.textContent = "O"
+  }
+  N += 1 
 }
 function btnClick8() {
-  btn8.textContent = "X"
+  if (N==0, N%2==0){
+    btn8.textContent = "X"
+  }
+  if (N%2!=0){
+    btn8.textContent = "O"
+  }
+  N += 1 
 }
 function btnClick9() {
-  btn9.textContent = "X"
+  if (N==0, N%2==0){
+    btn9.textContent = "X"
+  }
+  if (N%2!=0){
+    btn9.textContent = "O"
+  }
+  N += 1 
 }
+
+if (N==0, N%2==0){
+  ver.textContent = "X"
+}
+if (N%2!=0){
+  ver.textContent = "O"
+}
+
