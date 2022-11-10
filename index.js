f8aeb18c2b07041d100c97cbc9cee0d2ad56ac6a
Itamar <Itamarlucio19@gmail.com>
2022-11-10 17:15:15 -0300

    minor changes

--- index.js
+++ index.js
@@ -12,91 +12,104 @@ function btnR() {
 }
 
 let N = 0
+ver.textContent = "X"
 
 function btnClick1() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn1.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn1.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1 
 }
 function btnClick2() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn2.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn2.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1 
 }
 function btnClick3() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn3.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn3.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1 
 }
 function btnClick4() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn4.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn4.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1 
 }
 function btnClick5() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn5.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn5.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1 
 }
 function btnClick6() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn6.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn6.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1
 }
 function btnClick7() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn7.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn7.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1 
 }
 function btnClick8() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn8.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn8.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1 
 }
 function btnClick9() {
-  if (N==0, N%2==0){
+  if (N%2==0){
     btn9.textContent = "X"
+    ver.textContent = "O"
   }
   if (N%2!=0){
     btn9.textContent = "O"
+    ver.textContent = "X"
   }
   N += 1
-}
-if (N==0, N%2==0){
-  ver.textContent = "X"
-}
-if (N%2!=0){
-  ver.textContent = "O"
 }
